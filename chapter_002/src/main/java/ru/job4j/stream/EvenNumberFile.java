df0c7d0267f1263c5c9d49c985d911866569a570
AlexChereremisin <71m25@mail.ru>
2020-10-30 12:49:48 +0300

    0.2. FileInputStream [#300435]

---
+++ chapter_002/src/main/java/ru/job4j/stream/EvenNumberFile.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import java.io.FileInputStream;
+
+/**
+ * Класс для демонстрации считывания данных из файла.
+ */
+public class EvenNumberFile {
+    /**
+     * Точка входа в программу.
+     * @param args массив строк.
+     */
+    public static void main(String[] args) {
+        try(FileInputStream in = new FileInputStream("even.txt")) {
+            StringBuilder str = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                str.append((char) read);
+            }
+            for (String s : str.toString().split(System.lineSeparator())) {
+                if (Integer.parseInt(s) % 2 == 0) {
+                    System.out.println(s + " четное.");
+                } else {
+                    System.out.println(s + " нечетное.");
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
