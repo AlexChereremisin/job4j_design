e66393f0317b1d9a280c8fd9b0c673c0b4613eb0
AlexChereremisin <71m25@mail.ru>
2020-10-30 11:48:58 +0300

    0.1. FileOutputStream. [#300426]

---
+++ chapter_002/src/main/java/ru/job4j/stream/MatrixFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.stream;
+
+import java.io.FileOutputStream;
+
+/**
+ * Класс для демонстрации записи в файл посредством FileOutputStream.
+ */
+public class MatrixFile {
+    /**
+     * Точка входа в программу.
+     * @param args массив строк.
+     */
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix();
+        int[][] array = matrix.multiple(9);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            StringBuilder str = new StringBuilder();
+            for (int[] row : array) {
+                for (int col : row) {
+                    str.append(col).append("\t");
+                }
+                str.append("\n");
+                out.write(str.toString().getBytes());
+                str.delete(0, str.length() - 1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
