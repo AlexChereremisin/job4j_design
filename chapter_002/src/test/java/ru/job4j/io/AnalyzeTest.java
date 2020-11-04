0f01774cbbf054ea10ad0cd0d943651286d63f42
AlexChereremisin <71m25@mail.ru>
2020-11-04 09:16:26 +0300

    2. Анализ доступности сервера. [#300432] TemporaryFolder

--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,25 +1,32 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class AnalyzeTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenDiapasonExistsThenReadTarget() {
+    public void whenDiapasonExistsThenReadTarget() throws IOException {
+        File target = folder.newFile("target_test.txt");
         Analyze analyze = new Analyze();
         analyze.unavailable(
                 "./data/source_test.log",
-                "./data/target_test.txt"
+                target.getAbsolutePath()
         );
         try (
                 BufferedReader reader = new BufferedReader(
-                        new FileReader(
-                                "./data/target_test.txt")
+                        new FileReader(target.getAbsolutePath())
                 )
         ) {
             assertThat(reader.readLine(), is("10:57:01;10:59:01"));
