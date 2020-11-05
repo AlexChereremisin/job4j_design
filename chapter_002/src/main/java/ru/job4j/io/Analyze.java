efd0c9e32217baedb657afaf2e5613187379b80f
AlexChereremisin <71m25@mail.ru>
2020-11-05 11:52:52 +0300

    2. Анализ доступности сервера. [#300432]

--- chapter_002/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/io/Analyze.java
@@ -22,11 +22,22 @@ public class Analyze {
      *               400 или 500 на 200 или 300.
      */
     public void unavailable(String source, String target) {
-        try (
-                BufferedReader reader = new BufferedReader(new FileReader(source));
-                PrintWriter writer = new PrintWriter(new FileOutputStream(target))
-        ) {
-            StringBuilder str = new StringBuilder();
+        this.writeDiapason(target, this.findDiapason(source));
+    }
+
+    /**
+     * Метод поиска дипазонов времени, когда сервер недоступен.
+     * @param source файл с данными в виде
+     *               status time
+     *               где status - 200, 300, 400, 500.
+     *               А time - время в формате hh:mm:ss.
+     * @return стока вида start1;stop1#start2;stop2#...
+     *              где start - начало диапазона.
+     *              stop - конец диапазона.
+     */
+    private String findDiapason(String source) {
+        StringBuilder str = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             reader.lines().forEach(s -> {
                 if (s.startsWith("400") || s.startsWith("500")) {
                     if (str.toString().endsWith("#") || str.length() == 0) {
@@ -50,7 +61,26 @@ public class Analyze {
                     str.append(s.trim().split("\s")[1]).append("#");
                 }
             });
-            for (String diapason : str.toString().split("#")) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+    /**
+     * Метод записи дипазонов в файл.
+     * @param target в этот файл будут записаны диапазоны времени,
+     *              когда сервер был недоступен.
+     *              Начальное время - статус 500 или 400.
+     *              Конечное время - изменение статуса с
+     *              400 или 500 на 200 или 300.
+     * @param diapasons стока вида start1;stop1#start2;stop2#...
+     *              где start - начало диапазона.
+     *              stop - конец диапазона.
+     */
+    private void writeDiapason(String target, String diapasons) {
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
+            for (String diapason : diapasons.split("#")) {
                 writer.println(diapason);
             }
         } catch (Exception e) {
