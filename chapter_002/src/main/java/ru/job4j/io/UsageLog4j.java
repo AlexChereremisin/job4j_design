47aabcdcda3abe4564a0651c85f9a68deb898114
AlexChereremisin <71m25@mail.ru>
2021-02-16 17:30:08 +0300

    slf catch exception example

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,26 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Alexey Cheremisin";
-        int age = 33;
-        float weight = 82.3f;
-        short height = 175;
-        char gender= 'm';
-        long socialSecurityNumber = 12345678912345678L;
-        double bitcoinQuantity = 0.0000555;
-        byte children = 1;
-        boolean inMarriage = true;
-        LOG.debug(
-                "User info name : {}, age : {}, weight : {}, height : {}, gender : {}, social security number : {}, bitcoin quantity : {}, children : {}, marriage : {}",
-                name,
-                age,
-                weight,
-                height,
-                gender,
-                socialSecurityNumber,
-                bitcoinQuantity,
-                children,
-                inMarriage
-                );
+       try {
+           throw new Exception("Not supported code");
+       } catch (Exception e) {
+           LOG.error("Exception in log example", e);
+       }
     }
 }
